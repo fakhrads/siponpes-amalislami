a13a226b26915a02648e6dd454a66cffb7c23719
Fakhri Adi Saputra <fakhriadisaputra15@gmail.com>
2022-11-14 17:51:45 +0700

    Add login & logout

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,5 +1,6 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
+import Hash from '@ioc:Adonis/Core/Hash'
+import User from 'App/Models/User'
 export default class UsersController {
   public async index({}: HttpContextContract) {}
 
@@ -7,7 +8,29 @@ export default class UsersController {
 
   public async store({}: HttpContextContract) {}
 
-  public async show({}: HttpContextContract) {}
+  public async logout({ response, auth }: HttpContextContract) {
+    await auth.use('web').logout()
+    response.redirect('/login')
+  }
+
+  public async login({ auth, request, response }: HttpContextContract) {
+    const email = request.input('email')
+    const password = request.input('password')
+
+    // Lookup user manually
+    const user = await User
+      .query()
+      .where('email',email)
+      .firstOrFail()
+
+    // Verify password
+    if (!(await Hash.verify(user.password, password))) {
+      return response.badRequest('Invalid credentials')
+    }
+
+    // Create session
+    await auth.use('web').login(user)
+  }
 
   public async edit({}: HttpContextContract) {}
 
