6fc6983eac652c20ca2f184318222fc65e1b30d0
fakhrads <fakhriadisaputra15@gmail.com>
2022-11-24 23:32:28 +0700

    Some Changes

--- app/Controllers/Http/AchievementsController.ts
+++ app/Controllers/Http/AchievementsController.ts
@@ -4,9 +4,21 @@ export default class AchievementsController {
     public async index({ view }: HttpContextContract) {
         return view.render('admin/pages/achievements')
     }
-    public async store({ view, response, request }: HttpContextContract) {
-        
+    
+    public async create({ view }: HttpContextContract) {
+        return view.render('admin/pages/achievements_new')
+    }
+
+    public async store({ view}: HttpContextContract) {
         
         return view.render('admin/pages/achievements')
     }
+
+    public async show({}: HttpContextContract) {}
+
+    public async edit({}: HttpContextContract) {}
+
+    public async update({}: HttpContextContract) {}
+
+    public async destroy({}: HttpContextContract) {}
 }
