2b70b424f5de90d351c9c1b0e62a8c20f3b9fef3
fakhrads <fakhriadisaputra15@gmail.com>
2022-11-28 17:20:02 +0700

    Fix the error when Updating the data

--- app/Controllers/Http/PositionsController.ts
+++ app/Controllers/Http/PositionsController.ts
@@ -1,50 +1,53 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import MataPelajaran from 'App/Models/MataPelajaran'
+import Jabatan from 'App/Models/Jabatan'
 
-export default class SubjectsController {
+export default class PositionsController {
   public async index({ view }: HttpContextContract) {
-    const data = await MataPelajaran.all()
-    return view.render('admin/pages/subjects', {data: data})
+    const data = await Jabatan.all()
+    return view.render('admin/pages/', {data: data})
   }
 
   public async create({ view }: HttpContextContract) {
-    return view.render('admin/pages/subjects_new')
+    return view.render('admin/pages/position_new')
   }
 
-  public async store({ request, response, session, view}: HttpContextContract) {
-    const nama_pelajaran = request.input('nama_pelajaran')
+  public async store({ request, response, session }: HttpContextContract) {
+    const nama_jabatan = request.input('nama_jabatan')
+
     try {
-      await MataPelajaran.create({
-        nama_pelajaran: nama_pelajaran,
+      await Jabatan.create({
+        nama_jabatan: nama_jabatan
       })
-      session.flash('success','Data Mata Pelajaran Berhasil Ditambah')
-      return response.redirect().back()
-    } catch(e) {
-      session.flash('errors', e)
-      return view.render('errors/server-error')
+      session.flash('success','Data berhasil ditambah!')
+      response.redirect().back()
+    } catch (e) {
+      session.flash('error',e.message)
+      response.redirect().back()
     }
   }
 
   public async edit({ view, request, session, response }: HttpContextContract) {
     const id = request.param('id')
+    
     try {
-      const mata_pelajaran = await MataPelajaran.query().where('id',id).firstOrFail()
-      return view.render('admin/pages/subjects_edit', { data: mata_pelajaran })
+      const position = await Jabatan.query().where('id',id).firstOrFail()
+      return view.render('admin/pages/position_edit', {data: position})
     } catch(e) {
       session.flash('errors', e)
       //return response.json(e)
       return response.redirect().back()
     }
+
   }
 
   public async update({ request, response, session}: HttpContextContract) {
     const id = request.input('id')
-    const nama_pelajaran = request.input('nama_pelajaran')
+    const nama_jabatan = request.input('nama_pelajaran')
 
     try {
-      const subjects = await MataPelajaran.findOrFail(id)
+      const subjects = await Jabatan.findOrFail(id)
 
-      subjects.nama_pelajaran = nama_pelajaran
+      subjects.nama_jabatan = nama_jabatan
 
       await subjects.save()
 
@@ -56,10 +59,11 @@ export default class SubjectsController {
     }
   }
 
+
   public async destroy({ request, session, response}: HttpContextContract) {    
     const id = request.input('id')
     try {
-      const subjects = await MataPelajaran.findOrFail(id)
+      const subjects = await Jabatan.findOrFail(id)
       await subjects.delete()
 
       session.flash('success', "Data berhasil di hapus")
