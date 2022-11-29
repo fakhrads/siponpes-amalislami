93679c49cb1ca2bb22adf01202f944735f5a2c0f
fakhrads <fakhriadisaputra15@gmail.com>
2022-11-29 15:15:39 +0700

    Add photo_path with nullable()

--- database/migrations/1668578170705_blogs.ts
+++ database/migrations/1668578170705_blogs.ts
@@ -16,6 +16,7 @@ export default class extends BaseSchema {
         .unsigned()
         .references('users.id')
         .onDelete('CASCADE') 
+      table.string('photo_path').nullable()
       table.string('judul')
       table.string('content')
 
