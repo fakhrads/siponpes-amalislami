152e44005f6604d8b5f10495267f8a73fb8efa97
fakhrads <fakhriadisaputra15@gmail.com>
2022-12-15 16:07:10 +0700

    Upload Website.ts Model

---
+++ app/Models/Website.ts
@@ -0,0 +1,34 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Website extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public sejarah: string
+
+  @column()
+  public visi: string
+
+  @column()
+  public misi: string
+
+  @column()
+  public sambutan_ketua_yayasan: string
+
+  @column()
+  public photo_slide_satu: string
+
+  @column()
+  public photo_slide_dua: string
+
+  @column()
+  public photo_slide_tiga: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
